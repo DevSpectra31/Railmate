952854091a7b879eb319fa3631dcd89c3a229f85
Er. Vineet Rajpal <rajpalvineet800@gmail.com>
2025-12-04 10:49:45 +0530

    user registeration made in the backend

--- Backend/src/controllers/users.controllers.js
+++ Backend/src/controllers/users.controllers.js
@@ -1,7 +1,7 @@
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { User } from "../models/users.model.js";
+import { User } from "../models/Users.model.js";
 
 const registeruser = asyncHandler(async (req, res) => {
   console.log("----DEBUG----");
@@ -9,28 +9,29 @@ const registeruser = asyncHandler(async (req, res) => {
   //console.log("REQ FILE:", req.files);
   console.log("--------------");
 
-  const { username, email, fullname, passwordHash ,phone} = req.body;
+  const { username, email, name, password ,mobile,verified} = req.body;
 
-  if ([username, email, fullname, passwordHash,phone].some((field) => !field)) {
+  if ([username, email, name, password,mobile,verified].some((field) => !field)) {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = await User.findOne({ $or: [{ username }, { email },{phone}] });
+  const existedUser = await User.findOne({ $or: [{ username }, { email },{mobile}] });
   if (existedUser) {
-    throw new ApiError(409, "User with email or username already exists");
+    throw new ApiError(409, "User with email or username or mobile number already exists");
   }
 
       const user = await User.create({
         username,
         email,
-        fullname,
-        passwordHash,
-        phone,
+        name,
+        password,
+        mobile,
+        verified,
         //  avatar: avatarUpload.secure_url,
         //  coverImage: coverUpload?.secure_url || "",
       });
     
-       const createdUser = await User.findOne(user._id).select("-passwordHash");
+       const createdUser = await User.findOne(user._id).select("-password");
        if (!createdUser) {
          throw new ApiError(500, "Something went wrong while registering user");
        }
--- Backend/src/models/Users.model.js
+++ Backend/src/models/Users.model.js
@@ -1,4 +1,6 @@
 import mongoose ,{Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 const Userschema=new Schema(
     {
         username :{
@@ -18,6 +20,13 @@ const Userschema=new Schema(
             type:String,
             required:true,
         },
+        password:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim : true,
+        },
         mobile:{
             type:String,
             unique:true,
@@ -25,6 +34,7 @@ const Userschema=new Schema(
             index:true,
         },
         role:{
+            type:String,
             enum :["Customer","Staff","Vendor"],
             reuired:true,
         },
@@ -36,3 +46,38 @@ const Userschema=new Schema(
     {timestamps:true}
 )
 export const User=mongoose.model("User",Userschema);
+Userschema.pre("save",async function name(next) {
+    if(!this.isModified("password")) return next();
+    this.password=await bcrypt.hash(this.password,10);
+    next();
+});
+Userschema.methods.isPasswordCorrect=async function(password) {
+    return await bcrypt.compare(password,this.password);
+}
+Userschema.methods.isMobileCorrect=async function(mobile) {
+    return await bcrypt.compare(mobile,this.mobile)
+}
+Userschema.methods.generateAccessToKen=function(){
+    return jwt.sign(
+        {
+            _id:this.id,
+            username:this.username,
+            name:this.name,
+            email:this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+    );
+};
+Userschema.methods.generateRefreshToKen=function(){
+    return jwt.sign(
+        {
+            _id:this.id,
+            username:this.username,
+            name:this.name,
+            email:this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+    );
+};
